46f92bd7d97628001f27f5572a300bc825e437ed
phuc1nguyen <phuc1nguyen@proton.me>
2024-03-24 11:20:03 +0700

    Configure Capacitor for PWA

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'com.phuc1nguyen.budget',
+	appName: 'budget',
+	webDir: 'build',
+	server: {
+		androidScheme: 'build'
+	}
+};
+
+export default config;
